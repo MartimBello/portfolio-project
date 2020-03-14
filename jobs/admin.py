6626dbdcf868e113d71aa4277be430a1fb1b89eb
Martim Bello <martimbello@MBP-de-Martim.home>
2020-03-14 13:30:19 +0000

    Some changes to the home page

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import JobsConfig
+from .models import Job
 
-admin.site.register(JobsConfig)
+admin.site.register(Job)
